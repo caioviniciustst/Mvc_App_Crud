a160c8473f756ae1e1b67a0039ab5f5f5e4446dc
Caio <CAIO@DESKTOP-677HJBV>
2024-10-15 17:02:34 -0400

    Finalização do scarfoolding

---
+++ Mvc_App_Crud/Controllers/PedidoesController.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Mvc_App_Crud.Contexto;
+using Mvc_App_Crud.Models;
+
+namespace Mvc_App_Crud.Controllers
+{
+    public class PedidoesController : Controller
+    {
+        private readonly VendaContext _context;
+
+        public PedidoesController(VendaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Pedidoes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Pedidos.ToListAsync());
+        }
+
+        // GET: Pedidoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedidos
+                .FirstOrDefaultAsync(m => m.PedidoId == id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedido);
+        }
+
+        // GET: Pedidoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pedidoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PedidoId,DataPedido,Cliente")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pedido);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pedido);
+        }
+
+        // GET: Pedidoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedidos.FindAsync(id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+            return View(pedido);
+        }
+
+        // POST: Pedidoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PedidoId,DataPedido,Cliente")] Pedido pedido)
+        {
+            if (id != pedido.PedidoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pedido);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PedidoExists(pedido.PedidoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pedido);
+        }
+
+        // GET: Pedidoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedidos
+                .FirstOrDefaultAsync(m => m.PedidoId == id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedido);
+        }
+
+        // POST: Pedidoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pedido = await _context.Pedidos.FindAsync(id);
+            if (pedido != null)
+            {
+                _context.Pedidos.Remove(pedido);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PedidoExists(int id)
+        {
+            return _context.Pedidos.Any(e => e.PedidoId == id);
+        }
+    }
+}
